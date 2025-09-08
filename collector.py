7c4f9a6acaa5e74ea483b13df0dcaa5a7d3badcd
Tim Morgner <tim@timmorgner.de>
2025-09-08 21:35:00 +0200

    Füge Logging für Nutzeraktivitäten in Discord hinzu: Protokolliere, wenn Nutzer spielen, sowie Details zu ihrer Aktivität.

--- collector.py
+++ collector.py
@@ -32,6 +32,8 @@ class DataCollector:
                             tracked_users += 1
                             self.db.insert_discord_voice_activity(timestamp,str(member.id), channel.name, str(guild.id))
                             if member.activity:
+                                logging.info(f"User {member.name} is playing {member.activity}")
+                                logging.info(f"Activity details: {member.activity.name}, {member.activity.type}, {member.activity.state}, {member.activity.details} {member.activity.application_id}")
                                 self.db.insert_discord_game_activity(timestamp,str(member.id), str(member.activity))
                     if user_count > 0:
                         self.db.insert_discord_voice_channel(timestamp,
