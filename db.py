0dfee15a9ec6b85e596920b42c4f99251ddf218f
Tim Morgner <tim@timmorgner.de>
2025-09-08 11:31:05 +0200

    Füge collection_interval zu den Discord- und Steam-Aktivitätstabellen hinzu

--- db.py
+++ db.py
@@ -21,6 +21,7 @@ class Database:
                 discord_id TEXT,
                 channel_name TEXT,
                 guild_id TEXT
+                    ,collection_interval INTEGER DEFAULT NULL
             )
         ''')
         cursor.execute('''
@@ -30,6 +31,7 @@ class Database:
                 guild_id TEXT,
                 user_count INTEGER,
                 tracked_users INTEGER
+                    ,collection_interval INTEGER DEFAULT NULL
             )
         ''')
         cursor.execute('''
@@ -37,6 +39,7 @@ class Database:
                 timestamp INTEGER,
                 discord_id TEXT,
                 game_name TEXT
+                    ,collection_interval INTEGER DEFAULT NULL
             )
         ''')
         cursor.execute('''
@@ -44,6 +47,7 @@ class Database:
                 timestamp INTEGER,
                 steam_id TEXT,
                 game_name TEXT
+                    ,collection_interval INTEGER DEFAULT NULL
             )
         ''')
         connection.commit()
@@ -54,46 +58,45 @@ class Database:
     # Inserts
     #
 
-    def insert_discord_voice_channel(self,timestamp:float, channel_name: str, guild_id: str, user_count: int, tracked_users: int):
+    def insert_discord_voice_channel(self, timestamp: float, channel_name: str, guild_id: str, user_count: int, tracked_users: int):
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
         cursor.execute('''
-            INSERT INTO discord_voice_channels (timestamp, channel_name, guild_id, user_count, tracked_users)
-            VALUES (?, ?, ?, ?, ?)''', (timestamp, channel_name, guild_id, user_count, tracked_users))
+            INSERT INTO discord_voice_channels (timestamp, channel_name, guild_id, user_count, tracked_users, collection_interval)
+            VALUES (?, ?, ?, ?, ?, ?)''', (timestamp, channel_name, guild_id, user_count, tracked_users, DATA_COLLECTION_INTERVAL))
         connection.commit()
         connection.close()
-        logging.debug(f"Inserted Discord voice channel data: {channel_name}, {guild_id}, {user_count}, {tracked_users}")
+        logging.debug(f"Inserted Discord voice channel data: {channel_name}, {guild_id}, {user_count}, {tracked_users}, Interval: {DATA_COLLECTION_INTERVAL}")
 
-    def insert_discord_voice_activity(self,timestamp:float, discord_id: str, channel_name: str, guild_id: str):
+    def insert_discord_voice_activity(self, timestamp: float, discord_id: str, channel_name: str, guild_id: str):
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
         cursor.execute('''
-            INSERT INTO discord_voice_activity (timestamp, discord_id, channel_name, guild_id)
-            VALUES (?, ?, ?, ?)''', (timestamp, discord_id, channel_name, guild_id))
+            INSERT INTO discord_voice_activity (timestamp, discord_id, channel_name, guild_id, collection_interval)
+            VALUES (?, ?, ?, ?, ?)''', (timestamp, discord_id, channel_name, guild_id, DATA_COLLECTION_INTERVAL))
         connection.commit()
         connection.close()
-        logging.debug(f"Inserted Discord voice activity data: {discord_id}, {channel_name}, {guild_id}")
+        logging.debug(f"Inserted Discord voice activity data: {discord_id}, {channel_name}, {guild_id}, Interval: {DATA_COLLECTION_INTERVAL}")
 
-    def insert_discord_game_activity(self,timestamp:float, discord_id: str, game_name: str):
+    def insert_discord_game_activity(self, timestamp: float, discord_id: str, game_name: str):
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
         cursor.execute('''
-            INSERT INTO discord_game_activity (timestamp, discord_id, game_name)
-            VALUES (?, ?, ?)''', (timestamp, discord_id, game_name))
+            INSERT INTO discord_game_activity (timestamp, discord_id, game_name, collection_interval)
+            VALUES (?, ?, ?, ?)''', (timestamp, discord_id, game_name, DATA_COLLECTION_INTERVAL))
         connection.commit()
         connection.close()
-        logging.debug(f"Inserted Discord game activity data: {discord_id}, {game_name}")
+        logging.debug(f"Inserted Discord game activity data: {discord_id}, {game_name}, Interval: {DATA_COLLECTION_INTERVAL}")
 
-    def insert_steam_game_activity(self,timestamp:float, steam_id: str, game_name: str):
+    def insert_steam_game_activity(self, timestamp: float, steam_id: str, game_name: str):
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
         cursor.execute('''
-            INSERT INTO steam_game_activity (timestamp, steam_id, game_name)
-            VALUES (?, ?, ?)
-        ''', (timestamp, steam_id, game_name))
+            INSERT INTO steam_game_activity (timestamp, steam_id, game_name, collection_interval)
+            VALUES (?, ?, ?, ?)''', (timestamp, steam_id, game_name, DATA_COLLECTION_INTERVAL))
         connection.commit()
         connection.close()
-        logging.debug(f"Inserted Steam game activity data: {steam_id}, {game_name}")
+        logging.debug(f"Inserted Steam game activity data: {steam_id}, {game_name}, Interval: {DATA_COLLECTION_INTERVAL}")
 
     #
     # Queries
